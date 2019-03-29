b354725e309fbd9d903f58eb2280041da616340c
wuhan <com.cn.wuhan@gmail.com>
2019-03-29 14:57:15 +0800

    需求提出人:吴晗 需求内容:调整从store获取的pandect指标值为null时，不返回该指标的问题 开发人:吴晗 开发时间:2019-03-29

--- src/main/java/cn/uyun/controller/ResourceController.java
+++ src/main/java/cn/uyun/controller/ResourceController.java
@@ -4,6 +4,7 @@ import cn.uyun.bean.ResultExceptionJson;
 import cn.uyun.service.impl.ResourceServiceImpl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -104,7 +105,7 @@ public class ResourceController {
                 for (String ip : ips) {
                     sequencerResult.put(ip, result.get(ip));
                 }
-                return JSON.toJSONString(sequencerResult);
+                return JSON.toJSONString(sequencerResult, SerializerFeature.WriteMapNullValue);
             }
         }
     }
